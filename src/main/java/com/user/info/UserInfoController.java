72b825226e0e1602e25e6786b5c0d985cef47d79
Jeeyoun-S <dreaming990124@gmail.com>
2022-10-19 16:23:04 +0900

    update221019 - 통장 만들기 구현 (미완)

--- src/main/java/com/user/info/UserInfoController.java
+++ src/main/java/com/user/info/UserInfoController.java
@@ -1,11 +1,11 @@
-package com.user;
+package com.user.info;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,44 +13,35 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
 import com.user.account.UserAccountService;
-import com.user.info.UserInfoDto;
-import com.user.info.UserInfoService;
 
 @Controller
 @RequestMapping("user")
 @SessionAttributes("loginUser")
-public class UserController {
+public class UserInfoController {
 	
 	@Autowired
 	UserInfoService userInfoService;
 	
 	@Autowired
 	UserAccountService userAccountService;
-	
+
 	@PostMapping("join") // 회원가입
 	@ResponseBody
 	public Map<String, Object> joinUser(UserInfoDto userInfoDto) {
 		Map<String, Object> resultMap = userInfoService.joinUser(userInfoDto);
-
 		return resultMap;
 	}
 	
 	@PostMapping("login") // 로그인
 	@ResponseBody
-	public Map<String, Object> loginUser(UserInfoDto userInfoDto) {
+	public Map<String, Object> loginUser(UserInfoDto userInfoDto, Model model) {
 		Map<String, Object> resultMap = userInfoService.loginUser(userInfoDto);
 		
-		// 로그인 성공 시, 로그인 정보 세션에 넣어주기
-		if (resultMap.get("response").toString().equals("success_login")) {
-			setSession(userInfoDto);
+		if (resultMap.get("response").toString().equals("success_join")) {
+			model.addAttribute("loginUser", userInfoDto);
 		}
 		
 		return resultMap;
-	}
-	
-	@ModelAttribute("loginUser") // session에 로그인 데이터 저장
-	public UserInfoDto setSession(UserInfoDto userInfoDto) {
-		return userInfoDto;
 	}
 	
 	@PostMapping("logout") // 로그아웃
@@ -59,9 +50,8 @@ public class UserController {
 		sessionStatus.setComplete();
 		
 		Map<String, Object> resultMap = new HashMap<>();
-		
-		resultMap.put("response", "success_logout");
 		resultMap.put("contents", "로그아웃 되었습니다.");
+		resultMap.put("response", "success_logout");
 		
 		return resultMap;
 	}
--- src/main/java/com/user/info/UserInfoService.java
+++ src/main/java/com/user/info/UserInfoService.java
@@ -33,69 +33,69 @@ public class UserInfoService {
 		HashMap<String, Object> returnMap = new HashMap<>();
 		
 		String response = "";
-		String sentence = "";
+		String contents = "";
 		
 		// ÇÊ¼ö ÀÔ·Â°ª(id, password, name, nick_name, email, phone_number) Áß ÇÏ³ª¶óµµ ¾ø´Â °æ¿ì
 		if (id.equals("") || password.equals("") || name.equals("") || nick_name.equals("") || email.equals("") || phone_number.equals("")) {
 			response = "failure_empty_some";
-			sentence = "¾ÆÀÌµğ, ºñ¹Ğ¹øÈ£, ÀÌ¸§, ´Ğ³×ÀÓ, ÀÌ¸ŞÀÏ, ÀüÈ­¹øÈ£¸¦ ¸ğµÎ ÀÔ·ÂÇØ ÁÖ¼¼¿ä.";
+			contents = "¾ÆÀÌµğ, ºñ¹Ğ¹øÈ£, ÀÌ¸§, ´Ğ³×ÀÓ, ÀÌ¸ŞÀÏ, ÀüÈ­¹øÈ£¸¦ ¸ğµÎ ÀÔ·ÂÇØ ÁÖ¼¼¿ä.";
 		}
 		
 		// id (1)5~20±ÛÀÚ ¿µ¹®ÀÚ + ¼ıÀÚ (2)Áßº¹ È®ÀÎ 
 		else if (!Pattern.matches("^(?=.*[a-zA-z])(?=.*[0-9])(?!.*[^a-zA-z0-9]).{5,20}$", id)) {
 			response = "failure_wrong_format_id";
-			sentence = "¾ÆÀÌµğ´Â ¿µ¹®ÀÚ¿Í ¼ıÀÚ¸¦ ÇÏ³ª ÀÌ»ó Æ÷ÇÔÇØ 5 ~ 20ÀÚ·Î ÀÔ·ÂÇØ ÁÖ¼¼¿ä.";
+			contents = "¾ÆÀÌµğ´Â ¿µ¹®ÀÚ¿Í ¼ıÀÚ¸¦ ÇÏ³ª ÀÌ»ó Æ÷ÇÔÇØ 5 ~ 20ÀÚ·Î ÀÔ·ÂÇØ ÁÖ¼¼¿ä.";
 		} else if (findSameId(id) != null) {
 			response = "failure_duplicate_id";
-			sentence = "ÀÌ¹Ì Á¸ÀçÇÏ´Â ¾ÆÀÌµğÀÔ´Ï´Ù.";
+			contents = "ÀÌ¹Ì Á¸ÀçÇÏ´Â ¾ÆÀÌµğÀÔ´Ï´Ù.";
 		}
 		
 		// password (1)8~15±ÛÀÚ, ¿µ¹®ÀÚ + ¼ıÀÚ + Æ¯¼ö¹®ÀÚ (2)password¿Í password_confirm µ¿ÀÏ
 		else if (!Pattern.matches("^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[`~!@$!%*#^?&\\(\\)\\-_=+])(?!.*[^a-zA-z0-9`~!@$!%*#^?&\\(\\)\\-_=+]).{8,15}$", password)) {
 			response = "failure_wrong_format_pwd";
-			sentence = "ºñ¹Ğ¹øÈ£´Â ¿µ¹®ÀÚ, ¼ıÀÚ, Æ¯¼ö¹®ÀÚ¸¦ ÇÏ³ª ÀÌ»ó Æ÷ÇÔÇØ 8~15ÀÚ·Î ÀÔ·ÂÇØ ÁÖ¼¼¿ä.";
+			contents = "ºñ¹Ğ¹øÈ£´Â ¿µ¹®ÀÚ, ¼ıÀÚ, Æ¯¼ö¹®ÀÚ¸¦ ÇÏ³ª ÀÌ»ó Æ÷ÇÔÇØ 8~15ÀÚ·Î ÀÔ·ÂÇØ ÁÖ¼¼¿ä.";
 		} else if (!password.equals(password_confirm)) {
 			response = "failure_different_pwd";
-			sentence = "ºñ¹Ğ¹øÈ£ È®ÀÎÀÌ Æ²·È½À´Ï´Ù.";
+			contents = "ºñ¹Ğ¹øÈ£ È®ÀÎÀÌ Æ²·È½À´Ï´Ù.";
 		}
 		
 		// simple_pwd ¿Ï·á  (1)6ÀÚ¸®, ¼ıÀÚ
 		else if (!Pattern.matches("^[0-9]{6}$", simple_pwd)) {
 			response = "failure_wrong_format_simple_pwd";
-			sentence = "ÇÉ¹øÈ£´Â ¼ıÀÚ 6ÀÚ¸®·Î ÀÔ·ÂÇØ ÁÖ¼¼¿ä.";
+			contents = "ÇÉ¹øÈ£´Â ¼ıÀÚ 6ÀÚ¸®·Î ÀÔ·ÂÇØ ÁÖ¼¼¿ä.";
 		}
 		
 		// name (1)2~10±ÛÀÚ, ¹®ÀÚ
 		else if (!Pattern.matches("^[°¡-ÆR|a-z|A-Z]{2,10}$", name)) {
 			response = "failure_wrong_format_name";
-			sentence = "ÀÌ¸§Àº ¹®ÀÚ 2 ~ 10ÀÚ·Î ÀÔ·ÂÇØ ÁÖ¼¼¿ä.";
+			contents = "ÀÌ¸§Àº ¹®ÀÚ 2 ~ 10ÀÚ·Î ÀÔ·ÂÇØ ÁÖ¼¼¿ä.";
 		}
 		
 		// nickname (1)1~8±ÛÀÚ, ¹®ÀÚ, ¼ıÀÚ (2)Áßº¹ È®ÀÎ 
 		else if (!Pattern.matches("^[¤¡-¤¾|°¡-ÆR|a-z|A-Z|0-9]{1,8}$", nick_name)) {
 			response = "failure_wrong_format_nick_name";
-			sentence = "´Ğ³×ÀÓÀº ÇÑ±Û, ¿µ¾î, ¼ıÀÚ 1 ~ 8ÀÚ¸®·Î ÀÔ·ÂÇØ ÁÖ¼¼¿ä.";
+			contents = "´Ğ³×ÀÓÀº ÇÑ±Û, ¿µ¾î, ¼ıÀÚ 1 ~ 8ÀÚ¸®·Î ÀÔ·ÂÇØ ÁÖ¼¼¿ä.";
 		} else if (findSameNickName(nick_name) != null) {
 			response = "failure_duplicate_nick_name";
-			sentence = "ÀÌ¹Ì Á¸ÀçÇÏ´Â ´Ğ³×ÀÓÀÔ´Ï´Ù.";
+			contents = "ÀÌ¹Ì Á¸ÀçÇÏ´Â ´Ğ³×ÀÓÀÔ´Ï´Ù.";
 		}
 		
 		// email (1)ÀÌ¸ŞÀÏ Çü½Ä (2)Áßº¹ È®ÀÎ 
 		else if (!Pattern.matches("^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$", email)) {
 			response = "failure_wrong_format_email";
-			sentence = "ÀÌ¸ŞÀÏ Çü½Ä¿¡ ¸ÂÃç ÀÔ·ÂÇØ ÁÖ¼¼¿ä.";
+			contents = "ÀÌ¸ŞÀÏ Çü½Ä¿¡ ¸ÂÃç ÀÔ·ÂÇØ ÁÖ¼¼¿ä.";
 		} else if (findSameEmail(email) != null) {
 			response = "failure_duplicate_email";
-			sentence = "ÀÌ¹Ì ¾ÆÀÌµğ°¡ Á¸ÀçÇÏ´Â ÀÌ¸ŞÀÏÀÔ´Ï´Ù.";
+			contents = "ÀÌ¹Ì ¾ÆÀÌµğ°¡ Á¸ÀçÇÏ´Â ÀÌ¸ŞÀÏÀÔ´Ï´Ù.";
 		}
 		
 		// phone_number (1)10~11ÀÚ¸®, ¼ıÀÚ (2)Áßº¹ È®ÀÎ 
 		else if (!Pattern.matches("^[0-9]{10,11}$", phone_number)) {
 			response = "failure_wrong_format_phone_number";
-			sentence = "ÀüÈ­¹øÈ£´Â 01011112222Ã³·³ ¼ıÀÚ 11ÀÚ¸®·Î ÀÔ·ÂÇØ ÁÖ¼¼¿ä.";
+			contents = "ÀüÈ­¹øÈ£´Â 01011112222Ã³·³ ¼ıÀÚ 11ÀÚ¸®·Î ÀÔ·ÂÇØ ÁÖ¼¼¿ä.";
 		} else if (findSamePhoneNumber(phone_number) != null) {
 			response = "failure_duplicate_phone_number";
-			sentence = "ÀÌ¹Ì ¾ÆÀÌµğ°¡ Á¸ÀçÇÏ´Â ÀüÈ­¹øÈ£ÀÔ´Ï´Ù.";
+			contents = "ÀÌ¹Ì ¾ÆÀÌµğ°¡ Á¸ÀçÇÏ´Â ÀüÈ­¹øÈ£ÀÔ´Ï´Ù.";
 		}
 		
 		// address
@@ -105,16 +105,17 @@ public class UserInfoService {
 			String user_num = UUID.randomUUID().toString().replace("-", "");
 			userInfoDto.setUser_num(user_num);
 			
+			// È¸¿ø °¡ÀÔ ¼º°ø ½Ã, DB¿¡ È¸¿ø Á¤º¸ ³Ö±â
 			userInfoMapper.insertUser(userInfoDto);
-			
-			response = "success_join";
-			sentence = "È¸¿ø°¡ÀÔÀÌ ¿Ï·áµÆ½À´Ï´Ù.";
 			
 			// È¸¿ø°¡ÀÔ ¼º°ø ½Ã, ÅëÀå °³¼³ÇÏ±â
 			userAccountService.makeAccount(user_num);
+			
+			response = "success_join";
+			contents = "È¸¿ø°¡ÀÔÀÌ ¿Ï·áµÆ½À´Ï´Ù.";
 		}
 
-		returnMap.put("sentence", sentence);
+		returnMap.put("contents", contents);
 		returnMap.put("response", response);
 		
 		return returnMap;
@@ -128,35 +129,35 @@ public class UserInfoService {
 		HashMap<String, Object> returnMap = new HashMap<>();
 		
 		String response = "";
-		String sentence = "";
+		String contents = "";
 		
 		if (id.equals("") && password.equals("")) {
 			response = "failure_empty_id_pwd";
-			sentence = "¾ÆÀÌµğ¿Í ºñ¹Ğ¹øÈ£¸¦ ÀÔ·ÂÇØ ÁÖ¼¼¿ä.";
+			contents = "¾ÆÀÌµğ¿Í ºñ¹Ğ¹øÈ£¸¦ ÀÔ·ÂÇØ ÁÖ¼¼¿ä.";
 		} else if (id.equals("")) {
 			response = "failure_empty_id";
-			sentence = "¾ÆÀÌµğ¸¦ ÀÔ·ÂÇØ ÁÖ¼¼¿ä.";
+			contents = "¾ÆÀÌµğ¸¦ ÀÔ·ÂÇØ ÁÖ¼¼¿ä.";
 		} else if (password.equals("")) {
 			response = "failure_empty_password";
-			sentence = "ºñ¹Ğ¹øÈ£¸¦ ÀÔ·ÂÇØ ÁÖ¼¼¿ä.";
+			contents = "ºñ¹Ğ¹øÈ£¸¦ ÀÔ·ÂÇØ ÁÖ¼¼¿ä.";
 		} else {
 			// DB¿¡¼­ ÀÔ·ÂÇÑ id¿¡ ÇØ´çÇÏ´Â Á¤º¸ °¡Á®¿À±â
 			UserInfoDto selectUserInfoDto = userInfoMapper.selectUser(id);
 			
 			if (selectUserInfoDto == null) {
 				response = "failure_notExist_id";
-				sentence = "Á¸ÀçÇÏÁö ¾Ê´Â ¾ÆÀÌµğÀÔ´Ï´Ù.";
+				contents = "Á¸ÀçÇÏÁö ¾Ê´Â ¾ÆÀÌµğÀÔ´Ï´Ù.";
 			} else if (!selectUserInfoDto.getPassword().equals(password)) {
 				response = "failure_wrong_password";
-				sentence = "Àß¸øµÈ ºñ¹Ğ¹øÈ£ÀÔ´Ï´Ù.";
+				contents = "Àß¸øµÈ ºñ¹Ğ¹øÈ£ÀÔ´Ï´Ù.";
 			} else {
 				response = "success_login";
-				sentence = "·Î±×ÀÎÀÌ ¿Ï·áµÆ½À´Ï´Ù.";
+				contents = "·Î±×ÀÎÀÌ ¿Ï·áµÆ½À´Ï´Ù.";
 			}
 		}
 		
 		// API·Î ÀÀ´ä ³Ñ°ÜÁÖ±â
-		returnMap.put("sentence", sentence);
+		returnMap.put("contents", contents);
 		returnMap.put("response", response);
 		
 		return returnMap;
