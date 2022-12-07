cb8eebd7e29da1d6365d0f8efa2118063d6b1a33
hjlim <hjlim7831@gmail.com>
2022-12-07 22:29:09 +0900

    feat: stock/is-close 구현, interceptor쪽 리팩토링
    현 시각을 가져와 시간대와 비교하는 부분도 서비스로 넘기고,
    boolean 결과만 내는 메서드를 새로 만들어 인터셉터와 is-close에 사용

--- src/main/java/com/interceptor/StockCloseInterceptor.java
+++ src/main/java/com/interceptor/StockCloseInterceptor.java
@@ -1,8 +1,5 @@
 package com.interceptor;
 
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -21,17 +18,7 @@ public class StockCloseInterceptor implements HandlerInterceptor {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		// 매매 가능 시간 : 9:00 - 15:30
-		ZonedDateTime zt = ZonedDateTime.now(ZoneId.of("Asia/Seoul"));
-//		ZonedDateTime zt = ZonedDateTime.of(2022, 1, 1, 10, 0, 0, 0, ZoneId.of("Asia/Seoul"));
-		int year = zt.getYear();
-		int mo = zt.getMonthValue();
-		int day = zt.getDayOfMonth();
-
-		ZonedDateTime st = ZonedDateTime.of(year, mo, day, 9, 0, 0, 0, ZoneId.of("Asia/Seoul"));
-		ZonedDateTime ed = ZonedDateTime.of(year, mo, day, 15, 30, 0, 0, ZoneId.of("Asia/Seoul"));
-
-		if (openingDateComponent.isOpeningDate(zt) && zt.isAfter(st) && zt.isBefore(ed)) {
+		if (openingDateComponent.isOpen()) {
 			return true;
 		}
 
